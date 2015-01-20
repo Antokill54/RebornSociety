3173fc32de1fd5896888b6715c32f524148b947a
Antokill54 <a.lohner@outlook.com>
2015-01-20 11:29:59 +0100

    Fichier C++ & Cycle jour nuit + Fix
    Ajout des fichiers pour travailler en C++.
    Cycle jour nuit implémenté dans un blueprint mais désactivé pour le
    moment.
    Fix du problème qui faisait qu'on devait recompiler à chaque démarrage
    du projet.

---
+++ Source/RebornProject/RebornProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RebornProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RebornProject, "RebornProject" );
